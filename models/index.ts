765776b2a7cf850bcf54200610293f5516bc54f2
Konstantin Vasserman <kostya@amgix.io>
2026-03-06 16:27:50 -0500

    Initial commit

---
+++ models/index.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BulkUploadRequest';
+export * from './CollectionConfig';
+export * from './CustomDocumentVector';
+export * from './CustomVector';
+export * from './Document';
+export * from './DocumentStatus';
+export * from './DocumentStatusResponse';
+export * from './HTTPValidationError';
+export * from './MetadataFilter';
+export * from './MetadataIndex';
+export * from './OkResponse';
+export * from './QueueInfo';
+export * from './SearchQuery';
+export * from './SearchResult';
+export * from './ValidationError';
+export * from './ValidationErrorLocInner';
+export * from './Vector';
+export * from './VectorConfig';
+export * from './VectorScore';
+export * from './VectorSearchWeight';
+export * from './VersionResponse';
